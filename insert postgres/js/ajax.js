fc9c88c77502b839c7c62c830a0760df803df701
Bryan <bryan96ch@hotmail.com>
2024-09-27 12:05:43 -0500

    Agregando controles en el front

--- insert postgres/js/ajax.js
+++ insert postgres/js/ajax.js
@@ -1,22 +1,37 @@
-function guardar(fmr, e){
-    e.preventDefault();
-    let nombre = fmr.elements.nombre.value;
-    let civil = fmr.elements.civil.value;
-    let genero = fmr.elements.genero.value;
-    fetch('data.php', {
-        method: 'POST',
-        headers: {
-            'Accept': 'text/plain',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `nombre=${encodeURIComponent(nombre)}&civil=${encodeURIComponent(civil)}&genero=${encodeURIComponent(genero)}`
-    })
-    .then(res => res.text())
-    .then(res => {
-        console.log(res);
-        let div = document.getElementById('resultado');
-        div.innerHTML = res;
-    })
-    .catch(error => console.log('Error: ', error)
-    );
-}
+document.addEventListener('DOMContentLoaded', function () {
+    function guardar(e, fmr){
+        e.preventDefault();
+        const values = {
+            'nombre': fmr.elements.nombre.value,
+            'civil': fmr.elements.civil.value,
+            'genero': fmr.elements.genero.value
+        }
+        let body = [];
+        for (const key in values) {
+            if (values.hasOwnProperty(key) && values[key] !== '') {
+                body.push(`${key}=${encodeURIComponent(values[key])}`);
+            }else{
+                M.toast({html: '¡Llene todos los<br>campos obligatorios <span class="red-text">*</span>!', classes: 'rounded'});
+                return;
+            }
+        }
+        fetch('data.php', {
+            method: 'POST',
+            headers: {
+                'Accept': 'text/plain',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: body.join('&')
+        })
+        .then(res => res.text())
+        .then(res => {
+            console.log(res);
+            let div = document.getElementById('resultado');
+            div.innerHTML = res;
+        })
+        .catch(error => console.log('Error: ', error)
+        );
+    }
+    const FMR = document.getElementById('fmr');
+    FMR.addEventListener('submit', (e) => guardar(e, FMR));
+});
